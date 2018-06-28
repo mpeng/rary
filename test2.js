06f38d71283463f1a16af4fcad725a9b2259184d
mpeng <wensheng.peng@gmail.com>
2018-06-27 20:19:46 -0400

    test2

--- test2.js
+++ test2.js
@@ -1,6 +1,6 @@
 origArray = ["B", "C", "B", "B", "B", "A", "D"]; 
 filterArray = ["D"];
-map = {};
+map = new Map();
 
 for ( i = 0; i < origArray.length; i++ ) {
 	console.log(origArray[i]);
@@ -9,3 +9,33 @@ for ( i = 0; i < origArray.length; i++ ) {
     }
 
 }
+
+
+var fib = function( n ) {
+	return n < 2 ? n : fib( n - 1 ) + fib( n - 2 );	
+}
+
+for ( i = 0; i <= 10; i++ ) {
+	console.log(i + ": " + fib( i ) );
+}
+
+var NumericTest = function(n) {
+	return {
+		showMe: function() {
+			console.log( n );			
+			return ((n % 2) == 0);			
+		},	
+		isEven: function( n ) {
+			return ((n % 2) == 0);			
+		},
+		isNegative: function( n ) {
+			return ( n < 0 );				
+		}	
+	}			
+}
+
+test = new NumericTest(4);
+
+console.log( test.showMe() );
+console.log( test.isEven( 4 ) );
+console.log( test.isNegative( -4 ) );
